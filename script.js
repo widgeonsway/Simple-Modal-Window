da3fe0cf36018e5db8cdba207a10a1a3cf620544
Widgeon <91209299+widgeonsway@users.noreply.github.com>
2024-10-06 18:50:10 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (a) {
+  if (a.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
